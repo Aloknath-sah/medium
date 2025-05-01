f238caa3f630f493182c94834b014269ffa1944b
Aloknath-sah <kumaraditya6396@gmail.com>
2025-05-01 20:30:48 +0530

    added different blog and user routes and tested it in postman

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
+import {userRouter} from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
 	Bindings: {
@@ -9,81 +11,8 @@ const app = new Hono<{
     JWT_SECRET: string
 	}
 }>()
-
-
-
-app.post('api/v1/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
- 
-  const body = await c.req.json();
-
-  const user = await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password,
-
-    }
-  })
-  
-  const token = await sign({id: user.id}, c.env.JWT_SECRET)
-
-  return c.json({
-    jwt: token
-  })
-})
-
- app.post('api/v1/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
- 
-  const body = await c.req.json();
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-      password: body.password
-    }
-  })
-
-  if(!user) {
-    c.status(403);
-    c.json({error: "user not found"});
-  }
-
-  const jwt = await sign({id: user?.id}, c.env.JWT_SECRET)
-  return c.json({jwt})
-}) 
-
-app.use('/api/v1/blog/*', async (c, next) => {
-	const jwt = c.req.header('Authorization');
-	if (!jwt) {
-		c.status(401);
-		return c.json({ error: "unauthorized" });
-	}
-	const token = jwt.split(' ')[1];
-	const payload = await verify(token, c.env.JWT_SECRET);
-	if (!payload) {
-		c.status(401);
-		return c.json({ error: "unauthorized" });
-	}
-	c.set('userId', payload.id);
-	await next()
-})
-
- app.post('api/v1/blog', (c) => {
-  
-})
-
-app.put('api/v1/blog', (c) => {
-  
-}) 
-
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', (c) => c.text('Hono API is running!'))
+app.route('api/v1/user', userRouter)
+app.route('api/v1/blog', blogRouter)
 
 export default app
